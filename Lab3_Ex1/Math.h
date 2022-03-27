68d282d256bde776e713428955e059407d5a4e87
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-03-27 20:24:29 +0300

    I just coppied the class from the site

--- Lab3_Ex1/Math.h
+++ Lab3_Ex1/Math.h
@@ -3,16 +3,29 @@
 using namespace std;
 
 class Math
+
 {
+
 public:
+
     static int Add(int, int);
+
     static int Add(int, int, int);
+
     static int Add(double, double);
+
     static int Add(double, double, double);
+
     static int Mul(int, int);
+
     static int Mul(int, int, int);
+
     static int Mul(double, double);
+
     static int Mul(double, double, double);
-    static int Add(int count, ...);
+
+    static int Add(int count, ...); // sums up a list of integers
+
     static char* Add(const char*, const char*);
+
 };
