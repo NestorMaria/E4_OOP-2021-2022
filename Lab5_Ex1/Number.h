090bf0ec207f74e9ce4a1703885d010bb1345ec3
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-03-28 17:24:48 +0300

    I think the header is finished

--- Lab5_Ex1/Number.h
+++ Lab5_Ex1/Number.h
@@ -6,15 +6,85 @@ using namespace std;
 
 class Number
 {
-	//add data members
+private:
+	int base = 10, lenght;
+	char* value;
+
+	static int btoten(const char* value, int base);
+	static const char* tentob(int nr, int base);
+
 public:
 	Number(const char* value, int base);
 	~Number();
 
-	//add operators and copy/move constructor
+	Number(const Number& other);
+	Number(const char* value);
+	Number(int value);
+
 
 	void SwitchBase(int newBase);
 	void Print();
 	int GetDigitsCount();
 	int GetBase();
+
+	friend Number operator+(Number left, const Number& right)
+	{
+		left = left + right;
+		return left;
+	}
+
+	friend Number operator-(Number left, const Number& right)
+	{
+		left = left - right;
+		return left;
+	}
+
+	friend bool operator<(const Number& left, const Number& right)
+	{
+		int a, b;
+		a = btoten(left.value, left.base);
+		b = btoten(right.value, right.base);
+		return (a < b);
+	}
+
+	friend bool operator>(const Number& left, const Number& right)
+	{
+		int a, b;
+		a = btoten(left.value, left.base);
+		b = btoten(right.value, right.base);
+		return (a > b);
+	}
+
+	friend bool operator<=(const Number& left, const Number& right)
+	{
+		int a, b;
+		a = btoten(left.value, left.base);
+		b = btoten(right.value, right.base);
+		return (a <= b);
+	}
+
+	friend bool operator>=(const Number& left, const Number& right)
+	{
+		int a, b;
+		a = btoten(left.value, left.base);
+		b = btoten(right.value, right.base);
+		return (a >= b);
+	}
+
+	friend bool operator==(const Number& left, const Number& right)
+	{
+		int a, b;
+		a = btoten(left.value, left.base);
+		b = btoten(right.value, right.base);
+		return (a == b);
+	}
+
+	friend bool operator!=(const Number& left, const Number& right)
+	{
+		int a, b;
+		a = btoten(left.value, left.base);
+		b = btoten(right.value, right.base);
+		return (a != b);
+	}
+
 };
