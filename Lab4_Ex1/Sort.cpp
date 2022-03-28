7b3cb31a92f14b4a95b3bfaaf2bbf99fa9d9bf56
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-03-28 10:36:39 +0300

    I just finished writing the contructors

--- Lab4_Ex1/Sort.cpp
+++ Lab4_Ex1/Sort.cpp
@@ -107,3 +107,45 @@ int Sort::GetElementFromIndex(int index)
 	return -1;
 }
 
+Sort::Sort(int nrValues, int min, int max)
+{
+	this->nrValues = nrValues;
+	this->v = new int[this->nrValues];
+	for (int i = 0; i < this->nrValues; i++)
+		v[i] = rand() % (max - min) + min;
+}
+
+Sort::Sort(const Sort& other):Sort(other.nrValues,other.v){}
+
+Sort::Sort(int count, ...)
+{
+	this->nrValues = nrValues;
+	this->v = new int[this->nrValues];
+	va_list(args);
+	va_start(args, count);
+	for (int i = 0; i < count; i++)
+		this->v[i] = va_arg(args, int);
+	va_end(args);
+}
+
+Sort::Sort(const char* s)
+{
+	char* sir = new char[strlen(s) + 1];
+	strcpy(sir, s);
+	int number = 0;
+	for (int i = 0; sir[i]; i++)
+		if (sir[i] == ',')
+			number++;
+	this->nrValues = number + 1;
+	this->v = new int[this->nrValues];
+	char* p = strtok(sir, ",");
+	number = 0;
+	while (p)
+	{
+		int id = 0;
+		for (int i = 0; p[i]; i++)
+			id = id * 10 + p[i] - '0';
+		this->v[number++] = id;
+		p = strtok(NULL, ",");
+	}
+}
