9b0c33bd6def1da0bc451ca9889ce977a91ef0a3
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-03-28 09:59:46 +0300

    I just coppied the header

---
+++ Lab4_Ex1/Sort.h
@@ -0,0 +1,13 @@
+#pragma once
+class Sort
+{
+	//add data members
+public:
+	void InsertSort(bool ascendent=false);
+	void QuickSort(bool ascendent = false);
+	void BubbleSort(bool ascendent = false);
+	void Print();
+	int GetElementsCount();
+	int GetElementFromIndex(int index);
+	//add constructors
+};
