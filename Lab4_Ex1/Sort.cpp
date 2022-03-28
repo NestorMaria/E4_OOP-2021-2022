3930e6a0597dfaee62e1e89dc5c9c3f5f5b1ac24
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-03-28 10:52:20 +0300

    I had to change a little bit the files to define _CRT_SECURE_NO_WARNINGS and I finished the main.cpp file. The problem is solved

--- Lab4_Ex1/Sort.cpp
+++ Lab4_Ex1/Sort.cpp
@@ -1,151 +1,162 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "Sort.h"
+#include <stdlib.h>
+#include <cstdarg>
 
 void Sort::InsertSort(bool ascendent)
 {
-	for (int i = 1; i < nrValues; i++)
-	{
-		int temp = v[i];
-		int j = i - 1;
-		if (ascendent)
-		{
-			while (j >= 0 && temp < v[j])
-				v[j + 1] = v[j], j--;
-			v[j + 1] = temp;
-		}
-		else
-		{
-			while (j >= 0 && temp > v[j])
-				v[j + 1] = v[j], j--;
-			v[j + 1] = temp;
-		}
-	}
+    for (int i = 1; i < nrValues; i++)
+    {
+        int temp = v[i], j = i - 1;
+        if (ascendent)
+        {
+            while (j >= 0 && temp < v[j])
+                v[j + 1] = v[j], j--;
+            v[j + 1] = temp;
+        }
+        else
+        {
+            while (j >= 0 && temp > v[j])
+                v[j + 1] = v[j], j--;
+            v[j + 1] = temp;
+        }
+    }
 }
 
 int Pivot(int* v, int st, int dr, bool ascendent)
 {
-	int i, j, di, dj;
-	i = st; j = dr;
-	di = 0; dj = 1;
-	while (i < j)
-	{
-		if (ascendent)
-		{
-			if (v[i] > v[j])
-			{
-				v[i] = v[i] + v[j];
-				v[j] = v[i] - v[j];
-				v[i] = v[i] - v[j];
-				di = di + dj;
-				dj = di - dj;
-				di = di - dj;
-			}
-			i = i + di;
-			j = j - dj;
-		}
-		else
-		{
-			if (v[i] < v[j])
-			{
-				v[i] = v[i] + v[j];
-				v[j] = v[i] - v[j];
-				v[i] = v[i] - v[j];
-				di = di + dj;
-				dj = di - dj;
-				di = di - dj;
-			}
-			i = i + di;
-			j = j - dj;
-		}
-	}
-	return i;
+    int i, j, di, dj;
+    i = st;
+    j = dr;
+    di = 0;
+    dj = 1;
+    while (i < j)
+    {
+        if (ascendent)
+        {
+            if (v[i] > v[j])
+            {
+                v[i] += v[j];
+                v[j] = v[i] - v[j];
+                v[i] -= v[j];
+                di += dj;
+                dj = di - dj;
+                di -= dj;
+            }
+            i += di;
+            j -= dj;
+        }
+        else
+        {
+            if (v[i] < v[j])
+            {
+                v[i] += v[j];
+                v[j] = v[i] - v[j];
+                v[i] -= v[j];
+                di += dj;
+                dj = di - dj;
+                di -= dj;
+            }
+            i += di;
+            j -= dj;
+        }
+    }
+    return i;
 }
 
 void Sort::QuickSort(int st, int dr, bool ascendent)
 {
-	int p;
-	if (st <= dr)
-	{
-		p = Pivot(this->v, st, dr, ascendent);
-		QuickSort(st, p - 1, ascendent);
-		QuickSort(p + 1, dr, ascendent);
-	}
+    int p;
+    if (st <= dr)
+    {
+        p = Pivot(this->v, st, dr, ascendent);
+        QuickSort(st, p - 1, ascendent);
+        QuickSort(p + 1, dr, ascendent);
+    }
 }
 
 void Sort::BubbleSort(bool ascendent)
 {
-	bool sortat=false;
-	do
-	{
-		sortat = true;
-		for(int i=0;i<nrValues-1;i++)
-			if (v[i] > v[i + 1])
-			{
-				v[i] = v[i] + v[i + 1];
-				v[i + 1] = v[i] - v[i + 1];
-				v[i] = v[i] - v[i + 1];
-				sortat = false;
-			}
-	} while (!sortat);
+    bool sortat;
+    do {
+        sortat = true;
+        for (int i = 0; i < nrValues - 1; i++)
+            if (v[i] > v[i + 1])
+            {
+                v[i] += v[i + 1];
+                v[i + 1] = v[i] - v[i + 1];
+                v[i] -= v[i + 1];
+                sortat = false;
+            }
+    } while (!sortat);
 }
 
 void Sort::Print()
 {
-	for (int i = 0; i < nrValues; i++)
-		cout << v[i] << ' ';
-	cout << endl;
-}
-
-int Sort::GetElementsCount()
-{
-	return nrValues;
+    for (int i = 0; i < nrValues; i++)
+        cout << v[i] << ' ';
+    cout << '\n';
 }
 
 int Sort::GetElementFromIndex(int index)
 {
-	if (index > -1 && index < nrValues)
-		return v[index];
-	return -1;
+    if (index > -1 && index < nrValues)
+        return v[index];
+    return -1;
+}
+
+int Sort::GetElementsCount()
+{
+    return nrValues;
 }
 
 Sort::Sort(int nrValues, int min, int max)
 {
-	this->nrValues = nrValues;
-	this->v = new int[this->nrValues];
-	for (int i = 0; i < this->nrValues; i++)
-		v[i] = rand() % (max - min) + min;
+    this->nrValues = nrValues;
+    this->v = new int[this->nrValues];
+    for (int i = 0; i < this->nrValues; i++)
+        v[i] = rand() % (max - min) + min;
 }
 
-Sort::Sort(const Sort& other):Sort(other.nrValues,other.v){}
+Sort::Sort(const Sort& other) : Sort(other.nrValues, other.v) { }
+
+Sort::Sort(int nrValues, int* vect)
+{
+    this->nrValues = nrValues;
+    this->v = new int[this->nrValues];
+    for (int i = 0; i < this->nrValues; i++)
+        this->v[i] = vect[i];
+}
 
 Sort::Sort(int count, ...)
 {
-	this->nrValues = nrValues;
-	this->v = new int[this->nrValues];
-	va_list(args);
-	va_start(args, count);
-	for (int i = 0; i < count; i++)
-		this->v[i] = va_arg(args, int);
-	va_end(args);
+    this->nrValues = count;
+    this->v = new int[this->nrValues];
+    va_list args;
+    va_start(args, count);
+    for (int i = 0; i < count; ++i)
+        this->v[i] = va_arg(args, int);
+    va_end(args);
 }
 
-Sort::Sort(const char* s)
+Sort::Sort(const char* sir)
 {
-	char* sir = new char[strlen(s) + 1];
-	strcpy(sir, s);
-	int number = 0;
-	for (int i = 0; sir[i]; i++)
-		if (sir[i] == ',')
-			number++;
-	this->nrValues = number + 1;
-	this->v = new int[this->nrValues];
-	char* p = strtok(sir, ",");
-	number = 0;
-	while (p)
-	{
-		int id = 0;
-		for (int i = 0; p[i]; i++)
-			id = id * 10 + p[i] - '0';
-		this->v[number++] = id;
-		p = strtok(NULL, ",");
-	}
+    char* s = new char[strlen(sir) + 1];
+    strcpy(s, sir);
+    int number = 0;
+    for (int i = 0; s[i]; i++)
+        if (s[i] == ',')
+            number++;
+    this->nrValues = number + 1;
+    this->v = new int[this->nrValues];
+    char* p = strtok(s, ",");
+    number = 0;
+    while (p)
+    {
+        int id = 0;
+        for (int i = 0; p[i]; i++)
+            id = id * 10 + p[i] - '0';
+        this->v[number++] = id;
+        p = strtok(NULL, ",");
+    }
 }
--- Lab4_Ex1/Sort.h
+++ Lab4_Ex1/Sort.h
@@ -1,27 +1,26 @@
 #pragma once
 #include <iostream>
-#include <string.h>
-#include <stdarg.h>
-#include <cstdlib>
 
 using namespace std;
 
 class Sort
 {
+    // add data members
 private:
-	int nrValues;
-	int* v;
+    int nrValues;
+    int* v;
+
+    // add constuctors
 public:
-	void InsertSort(bool ascendent=false);
-	void QuickSort(int st,int dr,bool ascendent = false);
-	void BubbleSort(bool ascendent = false);
-	void Print();
-	int GetElementsCount();
-	int GetElementFromIndex(int index);
-	
-	Sort(int nrValues, int min, int max);
-	Sort(const Sort& other);
-	Sort(int* v, int nrValues);
-	Sort(int count, ...);
-	Sort(const char* s);
+    void InsertSort(bool ascendent = false);
+    void QuickSort(int st, int dr, bool ascendent = false);
+    void BubbleSort(bool ascendent = false);
+    void Print();
+    int GetElementsCount();
+    int GetElementFromIndex(int index);
+    Sort(int nrValues, int min, int max);
+    Sort(const Sort& other);
+    Sort(int nrValues, int* v);
+    Sort(int count, ...);
+    Sort(const char* sir);
 };
