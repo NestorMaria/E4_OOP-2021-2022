bc1d9f31fe6a3c84e41afb76351881f04ebd7f6e
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-03-31 08:31:41 +0300

    I think the header files are done

---
+++ Lab6_Ex1/Ford.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Car.h"
+
+class Ford :public Car
+{
+public:
+	Ford();
+};
---
+++ Lab6_Ex1/Mazda.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Car.h"
+
+class Mazda :public Car
+{
+public:
+	Mazda();
+};
