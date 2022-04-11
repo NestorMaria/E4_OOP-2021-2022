5a621c340855df4c54fc862c97a30d6f825d86df
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-04-11 17:04:53 +0300

    I wrote the functions DrawLine() and SetPoint()

--- Lab3_Ex2/Canvas.cpp
+++ Lab3_Ex2/Canvas.cpp
@@ -13,3 +13,27 @@ Canvas::Canvas(int width, int height)
 		for (int j = 0; j < this->columns; j++)
 			matrix[i][j] = ' ';
 }
+
+void Canvas::SetPoint(int x, int y, char ch)
+{
+	matrix[x][y] = ch;
+}
+
+void Canvas::DrawLine(int x1, int y1, int x2, int y2, char ch)
+{
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int D = 2 * dy - dx;
+	int y = y1;
+
+	for (int x = x1; x < x2; x++)
+	{
+		SetPoint(x, y, ch);
+		if (D > 0)
+		{
+			y = y + 1;
+			D = D - 2 * dx;
+		}
+		D = D + 2 * dy;
+	}
+}
