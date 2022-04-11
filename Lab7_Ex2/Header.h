0e0262b2b291a520b4a4b110eb9e31e7d0a5c950
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-04-11 18:49:59 +0300

    Now the header is finnished (I forgot about resizing the vector)

--- Lab7_Ex2/Header.h
+++ Lab7_Ex2/Header.h
@@ -9,6 +9,16 @@ private:
 	int lenght;
 	int size;
 	A* items;
+
+	void Resizing()
+	{
+		int new_size = size * 2;
+		A* new_items = new A[new_size];
+		memcpy(new_items, items, size * sizeof(A));
+		this->size = new_size;
+		delete[] this->items;
+		this->items = new_items;
+	}
 public:
 	void Push(const A& x);
 	const A Pop();
