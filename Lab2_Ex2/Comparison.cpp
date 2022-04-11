5da0ace06adf5057439f35d1e38b58d6c134270e
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-04-11 15:00:47 +0300

    I think I finnished the Comparison.cpp file

--- Lab2_Ex2/Comparison.cpp
+++ Lab2_Ex2/Comparison.cpp
@@ -26,6 +26,38 @@ int Comparison::CmpMath(float x, float y)
 	else return - 1;
 }
 
-//CmpEng
-//CmpHst
-//CmpAvg
+int Comparison::CmpEng(float x, float y)
+{
+	Student s1, s2;
+	s1.SetEng(x);
+	s2.SetEng(y);
+
+	if (s1.GetEng() == s2.GetEng())
+		return 0;
+	else if (s1.GetEng() > s2.GetEng())
+		return 1;
+	else return -1;
+}
+
+int Comparison::CmpHst(float x, float y)
+{
+	Student s1, s2;
+	s1.SetHst(x);
+	s2.SetHst(y);
+
+	if (s1.GetHst() == s2.GetHst())
+		return 0;
+	else if (s1.GetHst() > s2.GetHst())
+		return 1;
+	else return -1;
+}
+
+int Comparison::CmpAvg(float x, float y)
+{
+	Student s1, s2;
+	if (s1.Avg() == s2.Avg())
+		return 0;
+	else if (s1.Avg() > s2.Avg())
+		return 1;
+	else return -1;
+}
