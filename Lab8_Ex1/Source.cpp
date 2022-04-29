ed25e1817dd9e678b17174448a46c9820edff535
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-04-29 13:47:41 +0300

    Commit no 2

--- Lab8_Ex1/Source.cpp
+++ Lab8_Ex1/Source.cpp
@@ -12,3 +12,26 @@ typedef pair<string, int>si;
 
 
 
+int main()
+{
+	getline(fin, s);
+	fin.close();
+	int cuv1 = s.find_first_of(sep);
+	int ucuv = 0;
+	while (cuv1 != string::npos)
+	{
+		if (cuv1 != ucuv)
+		{
+			string cuv = s.substr(ucuv, cuv1 - ucuv);
+			for (auto& i : cuv)
+			{
+				if (i <= 'Z' && i >= 'A')
+					i = i - 'A' - 'a';
+			}
+			ap[cuv]++;
+		}
+		ucuv = cuv1 + 1;
+		cuv1 = s.find_first_of(sep, ucuv);
+	}
+}
+
