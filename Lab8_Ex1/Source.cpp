b9cd304a71b66f7a11d088b52c32fd5b3cc76531
Maria Nestor <maria.nestor.classroom@gmail.com>
2022-04-29 13:43:00 +0300

    Changing almost everything

--- Lab8_Ex1/Source.cpp
+++ Lab8_Ex1/Source.cpp
@@ -3,45 +3,12 @@
 #include <queue>
 #include <vector>
 #include <iostream>
+#include <fstream>
 using namespace std;
+ifstream fin("Phrase.txt");
+string s, sep(" ,?!.");
+map<string, int>ap;
+typedef pair<string, int>si;
 
 
-string TOLOWER(string a)
-{
-	for (char& c : a)
-		c = tolower(c);
-	return a;
-}
-
-
-
-int main()
-{
-
-	string s = "I bought an apple. Then I eat an apple. Apple is my favourite.";
-	map<string, int>mp;
-	
-	auto position=s.find(" ");
-	while (!s.empty() && position!=EOF)
-	{
-		position = s.find(" ");
-		string aux = s.substr(0, position);
-		aux = TOLOWER(aux);
-
-
-		auto it = mp.find(aux);
-		if (it != mp.end())
-			it->second++;
-		mp.insert(pair<string, int>(aux, 1));
-		s.erase(0, position+1);
-	}
-
-
-	
-	for (auto it = mp.begin(); it != mp.end(); ++it)
-		cout << it->first << ' ' << it->second << endl;
-
-	
-	return 0;
-}
 
